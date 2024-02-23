a51fb960e5d4be0772bb91ba47d1739bbed94149
aidan073 <122903269+aidan073@users.noreply.github.com>
2024-02-22 23:19:42 -0500

    Update assignment2.py
    many changes

--- assignment2.py
+++ assignment2.py
@@ -18,20 +18,61 @@ class unigram:
             for file in os.listdir(path): # for song in genre
                 with open(path + "/" + file, 'r') as rfile: 
                     for line in rfile: # for line in song
-                        
-                        # prepare text for tokenization
-                        current_line = line.strip()
-                        current_line = current_line.lower()
-                        current_line = re.sub(r'[^\w\s]', '', current_line)
-
-                        token_list = word_tokenize(current_line)
-
+                        token_list = token_prep.get_tokens(line) # tokenize
                         for token in token_list:
-                            # update frequency for existing term or add a new term with frequency 1
-                            temp_dic[token] = temp_dic.get(token, 0) + 1
-            dic_genre_word_appearances[genre] = temp_dic
+                            temp_dic[token] = temp_dic.get(token, 0) + 1 # update frequency for existing term or add a new term with frequency 1
+
+            dic_genre_word_appearances[genre] = temp_dic # add inner dic to outer dic
 
         return dic_genre_word_appearances # i.e. {'Blues': {'had': 17, 'a': 56, 'bad': 25}}
+    
+class bigram:
+    """This method will iterate on all the files in the input sub-directories and for each genre calculates the 
+        # of word appearances
+        @param directory_path: directory path where all the directories of different genre are located
+        @return: dictionary with genre name as the key and the value as a dictionary (A). (A) is a dictionary with key:
+        word value: the # of appearances of the word in the genre."""
+    def read_files(self, directory_path):
+        dic_genre_pair_appearances = {}
+        for genre in os.listdir(directory_path): # for genre in lyrics folder
+            path = directory_path + "/" + genre
+            temp_dic = {} # inner dictionary with key: term value: appearances in the genre
+            for file in os.listdir(path): # for song in genre
+                with open(path + "/" + file, 'r') as rfile: 
+                    for line in rfile: # for line in song              
+                        # prepare text for tokenization
+                        token_list = token_prep.get_tokens(line) # tokenize
+                        token_list.insert(0, "<s>")
+                        token_list.append("</s>")
+    
+                        if not token_list: # if empty line, skip and go to next line
+                            continue
+
+                        #first_entry_key = "<s>" + " " + token_list[0]
+                        #temp_dic[first_entry_key] = temp_dic.get(first_entry_key, 0) + 1 # create first pair entry
+                        for i in range(len(token_list) - 1):
+                            key = token_list[i] + " " + token_list[i + 1]
+                            # update frequency for existing term or add a new term with frequency 1
+                            temp_dic[key] = temp_dic.get(key, 0) + 1
+                        #last_entry_key = token_list[-1] + " " + "</s>" 
+                        #temp_dic[last_entry_key] = temp_dic.get(last_entry_key, 0) + 1 # create last
+            dic_genre_pair_appearances[genre] = temp_dic
+
+        return dic_genre_pair_appearances # i.e. {'Blues': {'<s> had': 9, ...}}
+    
+class token_prep:
+    """
+    This method takes in a line of text and preps it for tokenization and then returns the token list
+    @param line: line of text to be tokenized
+    @return: token list
+    """
+    @staticmethod
+    def get_tokens(line):
+        current_line = line.strip()
+        current_line = current_line.lower()
+        current_line = re.sub(r'[^\w\s]', '', current_line)
+        token_list = word_tokenize(current_line)
+        return token_list
 
 class tf_idf_calc:
     def get_TF_values(self, dic_genre_word_appearances):
@@ -79,35 +120,99 @@ class tf_idf_calc:
         return dic_genre_tfidf
     
 class tester:
-    def classify(self, tfidf, text):
+    @staticmethod
+    def classify_unigram(tfidf, text, lamda_setter):
         results = {}
 
-        text = text.strip()
-        text = text.lower()
-        text = re.sub(r'[^\w\s]', '', text)
-        text = re.sub(r'\n', ' ', text)
-        text = text.split()
+        input = token_prep.get_tokens(text)
 
-        for word in text:
+        for word in input:
             for genre in tfidf:
-                #if word in tfidf[genre].keys():
                 if genre in results:
                     results[genre] += tfidf[genre].get(word, 0)
                 else:
                     results[genre] = tfidf[genre].get(word, 0)
-        return max(results, key=results.get)
+        if lamda_setter == False:
+            return max(results, key=results.get)
+        else:
+            return results
+    @staticmethod
+    def classify_bigram(tfidf, text, lambda_setter):
+        results = {}
 
+        text = text.split('/n')
+
+        for line in text:
+            line = token_prep.get_tokens(line)
+            line.insert(0, "<s>")
+            line.append("</s>")
+            for i in range(len(line) - 1):
+                for genre in tfidf:
+                    curr = line[i] + " " + line[i + 1]
+                    if genre in results:
+                        results[genre] += tfidf[genre].get(curr, 0)
+                    else:
+                        results[genre] = tfidf[genre].get(curr, 0)
+        if lambda_setter == False:
+            return max(results, key=results.get)
+        else:
+            return results
+    
+class lambdas:
+    def set_lambda(self, uni_dic_tfidf, bi_dic_tfidf, directory_path):
+        most_correct_predictions = 0
+        best_lambda = 0
+        for i in range(11):
+            lambda_value = i / 10.0
+            curr_correct_predictions = 0
+            for file in os.listdir(directory_path): # for song in validation set
+                with open(directory_path + "/" + file, 'r') as rfile: 
+                    actual_genre = rfile.readline()
+                    actual_genre = actual_genre.strip()
+                    text = rfile.read()
+                    unigram_results = tester.classify_unigram(uni_dic_tfidf, text, True)
+                    bigram_results = tester.classify_bigram(bi_dic_tfidf, text, True)
+                    combined_results = {}
+                    for genre in unigram_results:
+                        combined = lambda_value * unigram_results[genre] + (1 - lambda_value) * bigram_results[genre]
+                        combined_results[genre] = combined
+                    
+                    if max(combined_results, key=combined_results.get) == actual_genre:
+                        curr_correct_predictions+=1
+            # check if new most_correct_predictions
+            if most_correct_predictions < curr_correct_predictions:
+                most_correct_predictions = curr_correct_predictions
+                best_lambda = lambda_value     
+        return best_lambda
 
 def main():
-    text = """I've had every promise generationalrift, there's anger in my heart
-You don't know freedombald it's like, chaingar don't have a clue
-If you did you'd find yourselves doing the same gorebor"""
+    text = "You used to call me on my cell phone/nLate night when you need my love/nCall me on my cell phone"
+
+    # create instances of objects
     unigram_model = unigram()
+    bigram_model = bigram()
     tf_idf_calculations = tf_idf_calc()
-    testing = tester()
+    lambdas1 = lambdas()
+
+    # get token counts in genres
+    dic_genre_pair_count = bigram_model.read_files("Lyrics")
     dic_genre_word_count = unigram_model.read_files("Lyrics")
-    dic_genre_term_frequencies = tf_idf_calculations.get_TF_values(dic_genre_word_count)
-    dic_term_idfs = tf_idf_calculations.get_IDF_values(dic_genre_word_count)
-    dic_genre_tfidf = tf_idf_calculations.get_TFIDF_values(dic_genre_term_frequencies, dic_term_idfs)
-    classification = testing.classify(dic_genre_tfidf, text)
+
+    # calculate tfidf scores for unigram
+    uni_dic_tf = tf_idf_calculations.get_TF_values(dic_genre_word_count)
+    uni_dic_idf = tf_idf_calculations.get_IDF_values(dic_genre_word_count)
+    uni_dic_tfidf = tf_idf_calculations.get_TFIDF_values(uni_dic_tf, uni_dic_idf)
+
+    # calculate tfidf scores for bigram
+    bi_dic_tf = tf_idf_calculations.get_TF_values(dic_genre_pair_count)
+    bi_dic_idf = tf_idf_calculations.get_IDF_values(dic_genre_pair_count)
+    bi_dic_tfidf = tf_idf_calculations.get_TFIDF_values(bi_dic_tf, bi_dic_idf)
+
+    # classify input
+    classification_uni = tester.classify_unigram(uni_dic_tfidf, text, False)
+    classification_bi = tester.classify_bigram(bi_dic_tfidf, text, False)
+
+    # set lambdas
+    best_lambda = lambdas1.set_lambda(uni_dic_tfidf, bi_dic_tfidf, "Validation Set")
+
 main()
