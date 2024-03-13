6a03950d8dca91b79cca03b2121f1c0d22214c24
evankoenig124 <100381767+evankoenig124@users.noreply.github.com>
2024-03-13 17:41:13 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,1 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import csv
+import re
+
+def scrapesongs(path):
+    with open('trainingdata.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Song Name', 'Genre', 'Lyrics'])
+        for genre in os.listdir(path):
+            if genre != '.DS_Store':
+                if genre == 'Metal':
+                    html = requests.get('https://www.popvortex.com/music/charts/top-heavy-metal-songs.php')
+                else:
+                    html = requests.get(f'https://www.popvortex.com/music/charts/top-{genre.lower()}-songs.php')
+                soup = BeautifulSoup(html.text, features='html.parser')
+                song_spans = soup.find_all('cite', class_='title')
+                artist_spans = soup.find_all('em',class_='artist')
+                for i in range(len(song_spans)):
+                    song_name = re.sub(r'[^a-zA-Z]', '-', song_spans[i].text)
+                    artist_name = re.sub(r'[^a-zA-Z]', '-', artist_spans[i].text)
+                    lyricshtml = requests.get(f'https://genius.com/{artist_name}-{song_name}-lyrics')
+                    lyricsoup = BeautifulSoup(lyricshtml.text, features='html.parser')
+                    lyrics = lyricsoup.find('div', class_='Lyrics__Container-sc-1ynbvzw-1 kUgSbL')
+                    if song_name not in path:
+                        if lyrics:
+                            cleanedlyrics = lyrics.get_text(separator=' ')
+                            pattern = r'\[.*?\]'
+                            cleanedlyrics = re.sub(pattern, '', cleanedlyrics)
+                            csv_writer.writerow([song_name, artist_name,genre,cleanedlyrics])
+
+def main():
+    testsongspath = "/Users/evankoenig/Downloads/Test Songs"
+    scrapesongs(testsongspath)
+
+if __name__ == "__main__":
+    main()
 
